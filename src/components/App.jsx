55c84bcb13abd9a214e3d4fa393f94f31879cbd3
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-03-04 17:57:20 -0300

    changed background to a animated one that i created then changed z index of box and navheader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,19 +8,27 @@ import ProjectsCard from "./ProjectsCard";
 import "../style/App.css";
 
 const App = () => {
-
-    return ( 
-      <Router>
-        <NavHeader />
-        <main>
-          <Switch>
-            <Route exact path="/" component={HomeCard} />
-            <Route path="/aboutme" component={AboutMeCard} />
-            <Route exact path="/projects" component={ProjectsCard} />
-          </Switch>
-        </main>
-      </Router>
-    );
-  
-}
+  return (
+    <Router>
+      <NavHeader />
+      <main className="anim-background">
+        <Switch>
+          <Route exact path="/" component={HomeCard} />
+          <Route path="/aboutme" component={AboutMeCard} />
+          <Route exact path="/projects" component={ProjectsCard} />
+        </Switch>
+        <ul className="anim-area">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </main>
+    </Router>
+  );
+};
 export default App;
