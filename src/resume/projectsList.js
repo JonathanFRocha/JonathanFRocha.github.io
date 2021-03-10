c45814fd638acf042d5504675f77dfa59aa1f063
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-03-10 11:38:16 -0300

    updated projects, changed image width and heigth inside the modal and in the projects card

--- src/resume/projectsList.js
+++ src/resume/projectsList.js
@@ -1,7 +1,8 @@
 import project1Img from "../images/projectImages/lessonsLearned.jpg";
+import project2Img from "../images/projectImages/project2.jpg";
 export const projects = [
   {
-    title: "Lessons Learned 1",
+    title: "Lessons Learned",
     code: "https://github.com/tryber/sd-010-b-project-lessons-learned/pull/38",
     link: "https://jonathanfrocha.github.io/lessons_learned/",
     img: project1Img,
@@ -10,30 +11,30 @@ export const projects = [
       "I developed this website that contains a lot of information about what i learned at Trybe in the first three chapters. my website has positioned and stylized elements and in addition, it contains appropriate semantics so that it is accessible and better ranked by search engines.",
   },
   {
-    title: "Lessons Learned 2",
-    code: "https://github.com/tryber/sd-010-b-project-lessons-learned/pull/38",
-    link: "https://jonathanfrocha.github.io/lessons_learned/",
-    img: project1Img,
-    alt: "Lessons Learned project Image, click to know more about.",
+    title: "Pixel Art",
+    code: "https://github.com/tryber/sd-010-b-project-pixels-art/pull/107",
+    link: "https://jonathanfrocha.github.io/pixel_art/",
+    img: project2Img,
+    alt: "Pixel Art project image, click to know more about.",
     description:
-      "I developed this website that contains a lot of information about what i learned at Trybe in the first three chapters. my website has positioned and stylized elements and in addition, it contains appropriate semantics so that it is accessible and better ranked by search engines.",
+      "In this project, i implemented a pixel art editor. A palette of random colors and a frame made up of pixels were made available, with these two features people can paint whatever they want on the board! A button was also made available to enlarge the frame and another to clean it completely.",
   },
   {
-    title: "Lessons Learned 3",
-    code: "https://github.com/tryber/sd-010-b-project-lessons-learned/pull/38",
-    link: "https://jonathanfrocha.github.io/lessons_learned/",
-    img: project1Img,
-    alt: "Lessons Learned project Image, click to know more about.",
+    title: "Pixel Art",
+    code: "https://github.com/tryber/sd-010-b-project-pixels-art/pull/107",
+    link: "https://jonathanfrocha.github.io/pixel_art/",
+    img: project2Img,
+    alt: "Pixel Art project image, click to know more about.",
     description:
-      "I developed this website that contains a lot of information about what i learned at Trybe in the first three chapters. my website has positioned and stylized elements and in addition, it contains appropriate semantics so that it is accessible and better ranked by search engines.",
+      "In this project, i implemented a pixel art editor. A palette of random colors and a frame made up of pixels were made available, with these two features people can paint whatever they want on the board! A button was also made available to enlarge the frame and another to clean it completely.",
   },
   {
-    title: "Lessons Learned 4",
-    code: "https://github.com/tryber/sd-010-b-project-lessons-learned/pull/38",
-    link: "https://jonathanfrocha.github.io/lessons_learned/",
-    img: project1Img,
-    alt: "Lessons Learned project Image, click to know more about.",
+    title: "Pixel Art",
+    code: "https://github.com/tryber/sd-010-b-project-pixels-art/pull/107",
+    link: "https://jonathanfrocha.github.io/pixel_art/",
+    img: project2Img,
+    alt: "Pixel Art project image, click to know more about.",
     description:
-      "I developed this website that contains a lot of information about what i learned at Trybe in the first three chapters. my website has positioned and stylized elements and in addition, it contains appropriate semantics so that it is accessible and better ranked by search engines.",
+      "In this project, i implemented a pixel art editor. A palette of random colors and a frame made up of pixels were made available, with these two features people can paint whatever they want on the board! A button was also made available to enlarge the frame and another to clean it completely.",
   },
 ];
